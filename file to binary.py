13908d1b02699e98106c80b599e6c22a82dd4f7c
yargi100k <132078235+yargi100k@users.noreply.github.com>
2024-04-02 21:09:28 +0300

    Add files via upload

---
+++ file to binary.py
@@ -0,0 +1,15 @@
+def file_to_binary(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            text = file.read()
+        binary_data = ''.join(format(ord(char), '08b') for char in text)
+        return binary_data
+    except FileNotFoundError:
+        print(f"The '{file_path}' was not found")
+    except Exception as e:
+        print(f"Error: {e}")
+file_path = input("Enter the path to the text file: ")
+binary_data = file_to_binary(file_path)
+if binary_data:
+    print("Binary code:")
+    print(binary_data)
